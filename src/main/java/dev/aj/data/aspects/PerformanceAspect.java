5a3260b0d4676e9c786f6af50a6eea853707674f
amarj <abg@gmail.com>
2025-01-05 12:43:10 +1100

    Refactor PerformanceAspect for cleaner formatting and annotations.
    Reorganized imports for better readability and relocated specific annotations for consistency. Improved the display message generation for logging by replacing StringTemplate with String.format for simplicity and better clarity. These changes enhance maintainability and code clarity without altering functionality.

--- src/main/java/dev/aj/data/aspects/PerformanceAspect.java
+++ src/main/java/dev/aj/data/aspects/PerformanceAspect.java
@@ -1,19 +1,19 @@
 package dev.aj.data.aspects;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-import org.apache.commons.lang3.time.StopWatch;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Component
@@ -23,13 +23,14 @@ public class PerformanceAspect {
     NumberFormat formatter = NumberFormat.getInstance(Locale.ENGLISH);
 
     @Pointcut(value = "@annotation(dev.aj.data.aspects.LogTiming)")
-    public void performanceTiming() {}
+    public void performanceTiming() {
+    }
 
     @SneakyThrows
-    @Around(value = "performanceTiming()")
+    @Around(value = "@annotation(dev.aj.data.aspects.LogTiming)")
     public Object timingAdvice(ProceedingJoinPoint proceedingJoinPoint) {
         LogTiming logTimingAnnotation = ((MethodSignature) proceedingJoinPoint.getSignature()).getMethod()
-                                                                                     .getAnnotation(LogTiming.class);
+                .getAnnotation(LogTiming.class);
         if (Objects.isNull(logTimingAnnotation)) {
             return proceedingJoinPoint.proceed();
         } else {
@@ -39,9 +40,11 @@ public class PerformanceAspect {
             Object response = proceedingJoinPoint.proceed();
             stopWatch.stop();
             TimeUnit timeUnit = logTimingAnnotation.displayPerformanceInTimeUnit();
-            String displayMessage = StringTemplate.STR."""
-                    \{message} took \{formatter.format(stopWatch.getTime(timeUnit))} \{timeUnit} in total.
-                    """;
+
+            String displayMessage = String.format(
+                    "%s took: %s %s in total",
+                    message, formatter.format(stopWatch.getTime(timeUnit)), timeUnit);
+
             log.info(displayMessage);
             return response;
         }
